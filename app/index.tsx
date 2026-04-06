d0e851391e81d721e96ddf32efe85d9adfbdc279
anibalcoder <144500768+anibalcoder@users.noreply.github.com>
2026-04-06 09:36:17 -0500

    chore: reset project base and setup NativeWind

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const App = () => {
+  return (
+    <View>
+      <Text className="text-4xl font-bold text-red-500">App</Text>
+    </View>
+  );
+};
+
+export default App;
